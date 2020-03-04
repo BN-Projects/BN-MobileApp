2d6922aa8fe08c5b963c550d7ba85d2102307dd7
UtkuErdemir <56741781+UtkuErdemir@users.noreply.github.com>
2020-03-04 17:34:49 +0300

    Menu Changes
    Menu Changed and Card Fields Added.

--- BNMobile/App.js
+++ BNMobile/App.js
@@ -1,19 +1,59 @@
 import React from 'react';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { ApplicationProvider, IconRegistry,Layout, Text } from '@ui-kitten/components';
 import { mapping, light as lightTheme } from '@eva-design/eva';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import {DrawerMenu} from './DrawerMenu';
-
+import {NotificationsPage} from './Notifications'
+import { CardIOModule, CardIOUtilities } from 'react-native-awesome-card-io';
+import { CreditCardInput, LiteCreditCardInput } from "react-native-credit-card-input";
+import {
+  TouchableOpacity,
+} from 'react-native';
 const App = () => (
   <ApplicationProvider mapping={mapping} theme={lightTheme}>
+    <IconRegistry icons={EvaIconsPack} />
     <DrawerMenu renderPage={pageChange("Anasayfa")}></DrawerMenu>
   </ApplicationProvider>
 );
 const Page = ()=>(
-    <Text>aq</Text>
+  <Layout style={{flex:1,backgroundColor:'#cccccc'}}>
+        <TouchableOpacity onPress={()=>scanCard()}>
+          <Text>Scan card!</Text>
+        </TouchableOpacity>
+        <CreditCardInput
+              autoFocus
+
+              requiresName
+              requiresCVC
+              requiresPostalCode
+
+              labelStyle={{
+                color: "black",
+                fontSize: 12,
+              }}
+              inputStyle={{
+                fontSize: 16,
+                color: "black",
+              }}
+              validColor={"black"}
+              invalidColor={"red"}
+              placeholderColor={"darkgray"}/>
+  </Layout>
 );
+const scanCard = async () => {
+  try {
+    const card = await CardIOModule.scanCard()
+    alert(JSON.stringify(card))
+  } catch (err) {
+    console.log(err)
+  }
+}
 const pageChange=(pageName)=>{
   if(pageName=="Anasayfa"){
     return <Page></Page>
+  }
+  else if(pageName=="Kartlar"){
+    return <NotificationsPage></NotificationsPage>
   }
 }
 export default App;
