f708ae57ce1e941c2b2c31d879d6834d024ca260
ugurcan cakar <ucakar@lely.com>
2020-04-08 03:15:11 +0300

    update profile added
    update profile added

---
+++ BNMobile/redux/reducers/initialState.js
@@ -0,0 +1,12 @@
+import Beacon from "../../models/Beacon";
+
+export default {
+    currentPage:"Giriş Yap",
+    login:"a",
+    beacons:[],
+    beaconDetail:"",
+    beaconEdit:"",
+    profile:"",
+    profileEdit:"",
+}
+//default parametreler oluşturuldu
